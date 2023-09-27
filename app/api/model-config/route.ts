b85d7c9292b6b0c935fef7e0f41b9b308df5b108
H0llyW00dzZ <priv8@btz.pm>
2023-09-27 10:18:38 +0700

    Models Deployment in Azure
    [+] feat(route.ts): add support for DEFAULT_MODELS constant in model_list array

--- app/api/model-config/route.ts
+++ app/api/model-config/route.ts
@@ -1,10 +1,11 @@
 import { NextResponse } from "next/server";
+import { DEFAULT_MODELS } from "@/app/constant";
 
 async function handle() {
   // @ts-ignore
   let model_list = process.env.MODEL_LIST.split(",").map((v) => {
     return {
-      name: v,
+      name: DEFAULT_MODELS,
       available: true,
     };
   });
