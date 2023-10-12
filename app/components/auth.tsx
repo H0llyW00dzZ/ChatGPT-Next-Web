66e3b5eb1e20c6476f3af2a8ce9fd9f30e249e0e
H0llyW00dzZ <priv8@btz.pm>
2023-10-12 16:00:27 +0700

    Refactor & Feat UI Page [Auth Page]
    Ref : Add setting for HIDE_USER_API_KEY Yidadaa#2994
    
    [+] refactor(auth.tsx): refactor conditional rendering of input access code and token
    [+] feat(auth.tsx): add support for isApp and isSysHasOpenaiApiKey variables in conditional rendering

--- app/components/auth.tsx
+++ app/components/auth.tsx
@@ -7,28 +7,20 @@ import { useAccessStore } from "../store";
 import Locale from "../locales";
 
 import BotIcon from "../icons/bot.svg";
-import { useEffect } from "react";
 import { getClientConfig } from "../config/client";
 
 export function AuthPage() {
   const navigate = useNavigate();
-  const accessStore = useAccessStore();
+  const access = useAccessStore();
 
   const goHome = () => navigate(Path.Home);
-  const goChat = () => navigate(Path.Chat);
-  const resetAccessCode = () => {
-    accessStore.update((access) => {
-      access.openaiApiKey = "";
-      access.accessCode = "";
-    });
+  const resetAccessCode = () => { // refactor this for better readability of code
+    access.updateCode("");
+    access.updateToken("");
   }; // Reset access code to empty string
-
-  useEffect(() => {
-    if (getClientConfig()?.isApp) {
-      navigate(Path.Settings);
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  const goPrivacy = () => navigate(Path.PrivacyPage);
+  const isApp = getClientConfig()?.isApp;
+  const isSysHasOpenaiApiKey = getClientConfig()?.isSysHasOpenaiApiKey;
 
   return (
     <div className={styles["auth-page"]}>
@@ -39,39 +31,64 @@ export function AuthPage() {
       <div className={styles["auth-title"]}>{Locale.Auth.Title}</div>
       <div className={styles["auth-tips"]}>{Locale.Auth.Tips}</div>
 
-      <input
-        className={styles["auth-input"]}
-        type="password"
-        placeholder={Locale.Auth.Input}
-        value={accessStore.accessCode}
-        onChange={(e) => {
-          accessStore.update(
-            (access) => (access.accessCode = e.currentTarget.value),
-          );
-        }}
-      />
-      {!accessStore.hideUserApiKey ? (
+      {!isApp && ( // Conditionally render the input access code based on whether it's not an app
+        <>
+          {isSysHasOpenaiApiKey ? (
+            <>
+              <input
+                className={styles["auth-input"]}
+                type="password"
+                placeholder={Locale.Auth.Input}
+                value={access.accessCode}
+                onChange={(e) => {
+                  access.updateCode(e.currentTarget.value);
+                }}
+              />
+              <div className={styles["auth-tips"]}>{Locale.Auth.SubTips}</div>
+              <input
+              className={styles["auth-input"]}
+              type="password"
+              placeholder={Locale.Settings.Token.Placeholder}
+              value={access.token}
+              onChange={(e) => {
+                access.updateToken(e.currentTarget.value);
+              }}
+            />
+            </>
+          ) : (
+            <input
+              className={styles["auth-input"]}
+              type="password"
+              placeholder={Locale.Settings.Token.Placeholder}
+              value={access.token}
+              onChange={(e) => {
+                access.updateToken(e.currentTarget.value);
+              }}
+            />
+          )}
+        </>
+      )}
+
+      {isApp && ( // Conditionally render the input access token based on whether it's an app
         <>
           <div className={styles["auth-tips"]}>{Locale.Auth.SubTips}</div>
           <input
             className={styles["auth-input"]}
             type="password"
-            placeholder={Locale.Settings.Access.OpenAI.ApiKey.Placeholder}
-            value={accessStore.openaiApiKey}
+            placeholder={Locale.Settings.Token.Placeholder}
+            value={access.token}
             onChange={(e) => {
-              accessStore.update(
-                (access) => (access.openaiApiKey = e.currentTarget.value),
-              );
+              access.updateToken(e.currentTarget.value);
             }}
           />
         </>
-      ) : null}
+      )}
 
       <div className={styles["auth-actions"]}>
         <IconButton
           text={Locale.Auth.Confirm}
           type="primary"
-          onClick={goChat}
+          onClick={goPrivacy}
         />
         <IconButton
           text={Locale.Auth.Later}
