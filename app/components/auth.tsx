469b27ec08e68f3bdf682603366caadb65ff776b
H0llyW00dzZ <priv8@btz.pm>
2023-10-12 16:00:27 +0700

    Refactor & Feat UI Page [Auth Page]
    Ref : Add setting for HIDE_USER_API_KEY Yidadaa#2994
    
    [+] refactor(auth.tsx): refactor conditional rendering of input access code and token
    [+] feat(auth.tsx): add support for isApp and isSysHasOpenaiApiKey variables in conditional rendering

--- app/components/auth.tsx
+++ app/components/auth.tsx
@@ -19,6 +19,8 @@ export function AuthPage() {
     access.updateToken("");
   }; // Reset access code to empty string
   const goPrivacy = () => navigate(Path.PrivacyPage);
+  const isApp = getClientConfig()?.isApp;
+  const isSysHasOpenaiApiKey = getClientConfig()?.isSysHasOpenaiApiKey;
 
   return (
     <div className={styles["auth-page"]}>
@@ -29,31 +31,45 @@ export function AuthPage() {
       <div className={styles["auth-title"]}>{Locale.Auth.Title}</div>
       <div className={styles["auth-tips"]}>{Locale.Auth.Tips}</div>
 
-      {!getClientConfig()?.isApp && ( // Conditionally render the input access code based on whether it's an app
+      {!isApp && ( // Conditionally render the input access code based on whether it's not an app
         <>
-          <input
-            className={styles["auth-input"]}
-            type="password"
-            placeholder={Locale.Auth.Input}
-            value={access.accessCode}
-            onChange={(e) => {
-              access.updateCode(e.currentTarget.value);
-            }}
-          />
-          <div className={styles["auth-tips"]}>{Locale.Auth.SubTips}</div>
-          <input
-            className={styles["auth-input"]}
-            type="password"
-            placeholder={Locale.Settings.Token.Placeholder}
-            value={access.token}
-            onChange={(e) => {
-              access.updateToken(e.currentTarget.value);
-            }}
-          />
+          {isSysHasOpenaiApiKey ? (
+            <>
+              <input
+                className={styles["auth-input"]}
+                type="password"
+                placeholder={Locale.Auth.Input}
+                value={access.accessCode}
+                onChange={(e) => {
+                  access.updateCode(e.currentTarget.value);
+                }}
+              />
+              <div className={styles["auth-tips"]}>{Locale.Auth.SubTips}</div>
+              <input
+              className={styles["auth-input"]}
+              type="password"
+              placeholder={Locale.Settings.Token.Placeholder}
+              value={access.token}
+              onChange={(e) => {
+                access.updateToken(e.currentTarget.value);
+              }}
+            />
+            </>
+          ) : (
+            <input
+              className={styles["auth-input"]}
+              type="password"
+              placeholder={Locale.Settings.Token.Placeholder}
+              value={access.token}
+              onChange={(e) => {
+                access.updateToken(e.currentTarget.value);
+              }}
+            />
+          )}
         </>
       )}
 
-      {getClientConfig()?.isApp && ( // Conditionally render the input access token based on whether it's an app
+      {isApp && ( // Conditionally render the input access token based on whether it's an app
         <>
           <div className={styles["auth-tips"]}>{Locale.Auth.SubTips}</div>
           <input
