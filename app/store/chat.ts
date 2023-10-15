25c4ecbd669260ab18908e0febc3a4086ecb637c
H0llyW00dzZ <priv8@btz.pm>
2023-10-16 05:03:34 +0700

    Add Models DALL-E-2
    [+] feat(constant.ts): add DALL-E-2 model to the list of default models
    [+] feat(chat.ts): add DALL-E-2 model for topic summarization and message summarization
    [+] feat(openai.ts): add user message for DALL-E-2 model and generate image description

--- app/store/chat.ts
+++ app/store/chat.ts
@@ -495,7 +495,7 @@ export const useChatStore = createPersistStore(
       
           const topicModel = getSummarizeModel(session.mask.modelConfig.model);
       
-          if (topicModel === "DALL-E-2-BETA-INSTRUCT-0613") {
+          if (topicModel === "DALL-E-2-BETA-INSTRUCT-0613" || "DALL-E-2") {
             // Summarize topic using gpt-3.5-turbo-0613 which is compatible with DALL-E-2 model
             api.llm.chat({
               messages: topicMessages,
@@ -582,7 +582,7 @@ export const useChatStore = createPersistStore(
         ) {
           const summarizeModel = getSummarizeModel(session.mask.modelConfig.model);
 
-          if (summarizeModel === "DALL-E-2-BETA-INSTRUCT-0613") {
+          if (summarizeModel === "DALL-E-2-BETA-INSTRUCT-0613" || "DALL-E-2") {
             // Summarize using gpt-3.5-turbo-0613 which is compatible with DALL-E-2 model
             api.llm.chat({
               messages: toBeSummarizedMsgs.concat(
