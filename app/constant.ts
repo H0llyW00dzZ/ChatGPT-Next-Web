00b5f4fc15c4273d9cbc416e83783efe977cfee2
H0llyW00dzZ <priv8@btz.pm>
2023-10-21 02:38:29 +0700

    Fix Client App [Tauri]
    [+] fix(constant.ts): add trailing slash to DEFAULT_API_HOST constant

--- app/constant.ts
+++ app/constant.ts
@@ -9,8 +9,7 @@ export const FETCH_TAG_URL = `https://api.github.com/repos/${OWNER}/${REPO}/tags
 export const RUNTIME_CONFIG_DOM = "danger-runtime-config";
 
 export const DEFAULT_CORS_HOST = "https://chatgpt.btz.sh";
-export const DEFAULT_API_HOST = `${DEFAULT_CORS_HOST}/api/proxy`;
-export const OPENAI_BASE_URL = "https://api.openai.com";
+export const DEFAULT_API_HOST = `${DEFAULT_CORS_HOST}/api/proxy/`;
 
 export enum Path {
   Home = "/",
@@ -25,12 +24,10 @@ export enum Path {
 
 export enum ApiPath {
   Cors = "/api/cors",
-  OpenAI = "/api/openai",
 }
 
 export enum SlotID {
   AppBody = "app-body",
-  CustomModel = "custom-model",
 }
 // This will automatically generate JSON files without the need to include the ".json" extension.
 export enum FileName {
@@ -64,11 +61,6 @@ export const REQUEST_TIMEOUT_MS = 60000;
 
 export const EXPORT_MESSAGE_CLASS_NAME = "export-markdown";
 
-export enum ServiceProvider {
-  OpenAI = "OpenAI",
-  Azure = "Azure",
-}
-
 export const OpenaiPath = {
   ChatPath: "v1/chat/completions",
   // text moderation
@@ -87,10 +79,6 @@ export const OpenaiPath = {
   ListModelPath: "v1/models",
 };
 
-export const Azure = {
-  ExampleEndpoint: "https://{resource-url}/openai/deployments/{deploy-id}",
-};
-
 export const DEFAULT_INPUT_TEMPLATE = `{{input}}`; // input / time / model / lang
 export const DEFAULT_SYSTEM_TEMPLATE = `
 You are ChatGPT, a large language model trained by OpenAI.
@@ -103,6 +91,7 @@ export const SUMMARIZE_MODEL = "gpt-3.5-turbo";
 
 export const KnowledgeCutOffDate: Record<string, string> = {
   default: "2021-09",
+  "gpt-3.5-turbo-1106": "2023-04",
   "gpt-4-1106-preview": "2023-04",
   "gpt-4-vision-preview": "2023-04",
 };
