18a7d6facd9f64cdc5a862bf83864572fe83b272
H0llyW00dzZ <priv8@btz.pm>
2023-10-29 13:23:16 +0700

    Fix UI Page [Layout] [All Device]
    [+] chore(layout.tsx): refactor themeColor to be part of viewport object
    [+] fix(layout.tsx): remove redundant meta tag for theme-color

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,13 +3,17 @@ import "./styles/globals.scss";
 import "./styles/markdown.scss";
 import "./styles/highlight.scss";
 import { getClientConfig } from "./config/client";
-import { type Metadata } from "next";
+import { type Metadata, type Viewport } from "next";
 
-
-export const themeColor = [
-  { media: "(prefers-color-scheme: light)", color: "#fafafa" },
-  { media: "(prefers-color-scheme: dark)", color: "#151515" },
-];
+export const viewport: Viewport = {
+    width: "device-width",
+    initialScale: 1,
+    maximumScale: 1,
+    themeColor: [
+      { media: "(prefers-color-scheme: light)", color: "#fafafa" },
+      { media: "(prefers-color-scheme: dark)", color: "#151515" },
+    ],
+};
 
 export const metadata: Metadata = {
   title: "ChatGPT Next Web",
@@ -31,7 +35,6 @@ export default function RootLayout({
         <meta name="config" content={JSON.stringify(getClientConfig())} />
         <link rel="manifest" href="/site.webmanifest"></link>
         <script src="/serviceWorkerRegister.js" defer></script>
-        <meta name="theme-color" content={JSON.stringify(themeColor)} />
       </head>
       <body>{children}</body>
     </html>
