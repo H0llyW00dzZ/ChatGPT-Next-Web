211beea071fa271efa5f48ad92a8399b5c20588f
H0llyW00dzZ <priv8@btz.pm>
2023-11-02 02:05:04 +0700

    CloudSync [Golang Sync] [WIP]
    [+] feat(settings.tsx): add support for GoSync provider in SyncConfigModal component
    [+] feat(locales): add localization support for GoSync provider in cn.ts, en.ts, and id.ts
    [+] feat(sync.ts): add gosync property to sync store and update version to 1.2
    [+] feat(cloud): add GoSync client implementation in gosync.ts
    [+] feat(cloud): add GoSync provider to SyncClients in index.ts

--- app/components/settings.tsx
+++ app/components/settings.tsx
@@ -565,6 +565,11 @@ function SyncConfigModal(props: { onClose?: () => void }) {
             </List>
           </>
         )}
+        {syncStore.provider === ProviderType.GoSync && (
+          <List>
+            <ListItem title={Locale.WIP}></ListItem>
+          </List>
+        )}
       </Modal>
     </div>
   );
