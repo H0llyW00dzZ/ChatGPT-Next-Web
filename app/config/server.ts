a885e979b40d125813ae476d725ca9074335735d
H0llyW00dzZ <priv8@btz.pm>
2023-11-10 03:28:05 +0700

    Refactor Server Configuration
    [+] chore(server.ts): refactor server configuration file

--- app/config/server.ts
+++ app/config/server.ts
@@ -1,21 +1,32 @@
 import binary from "spark-md5";
+import { DEFAULT_MODELS } from "../constant";
 
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
+      PROXY_URL?: string; // docker only
+
       OPENAI_API_KEY?: string;
       CODE?: string;
+
       BASE_URL?: string;
-      MODEL_LIST?: string;
-      PROXY_URL?: string;
+      OPENAI_ORG_ID?: string; // openai only
+
       VERCEL?: string;
-      VERCEL_ANALYTICS?: boolean; // vercel web analytics
+      BUILD_MODE?: "standalone" | "export";
+      BUILD_APP?: string; // is building desktop app
+      VERCEL_ANALYTICS?: string; // vercel web analytics
       HIDE_USER_API_KEY?: string; // disable user's api key input
       DISABLE_GPT4?: string; // allow user to use gpt-4 or not
       DISABLE_CUSTOMMODELS?: boolean; // allow user to use custom models or not
-      BUILD_MODE?: "standalone" | "export";
-      BUILD_APP?: string; // is building desktop app
-      HIDE_BALANCE_QUERY?: string; // allow user to query balance or not
+      ENABLE_BALANCE_QUERY?: string; // allow user to query balance or not
+      DISABLE_FAST_LINK?: string; // disallow parse settings from url or not
+      CUSTOM_MODELS?: string; // to control custom models
+
+      // azure only
+      AZURE_URL?: string; // https://{azure-url}/openai/deployments/{deploy-name}
+      AZURE_API_KEY?: string;
+      AZURE_API_VERSION?: string;
     }
   }
 }
@@ -53,19 +64,38 @@ export const getServerSideConfig = () => {
     apiKeys.set(hashedCode, apiKeyValue);
   });
 
+  const disableGPT4 = !!process.env.DISABLE_GPT4;
+  let customModels = process.env.CUSTOM_MODELS ?? "";
+
+  if (disableGPT4) {
+    if (customModels) customModels += ",";
+    customModels += DEFAULT_MODELS.filter((m) => m.name.startsWith("gpt-4"))
+      .map((m) => "-" + m.name)
+      .join(",");
+  }
+
+  const isAzure = !!process.env.AZURE_URL;
+
   return {
+    baseUrl: process.env.BASE_URL,
     apiKey,
-    code: process.env.CODE,
+    openaiOrgId: process.env.OPENAI_ORG_ID,
+
+    isAzure,
+    azureUrl: process.env.AZURE_URL,
+    azureApiKey: process.env.AZURE_API_KEY,
+    azureApiVersion: process.env.AZURE_API_VERSION,
+
     codes,
     needCode,
-    baseUrl: process.env.BASE_URL,
     proxyUrl: process.env.PROXY_URL,
     isVercel: !!process.env.VERCEL,
     isVercelWebAnalytics: !!process.env.VERCEL_ANALYTICS,
     hideUserApiKey: !!process.env.HIDE_USER_API_KEY,
-    disableGPT4: !!process.env.DISABLE_GPT4,
-    disableCustomModels: !!process.env.DISABLE_CUSTOMMODELS,
-    hideBalanceQuery: !!process.env.HIDE_BALANCE_QUERY,
+    disableGPT4,
+    hideBalanceQuery: !process.env.ENABLE_BALANCE_QUERY,
+    disableFastLink: !!process.env.DISABLE_FAST_LINK,
+    customModels,
     apiKeys,
   };
 };
