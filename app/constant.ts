de79bf6d7e8b0f736ed9cc60d779971fa7b0727b
H0llyW00dzZ <priv8@btz.pm>
2023-11-10 03:08:01 +0700

    Fix Constant
    [+] chore(constant.ts): fix OPENAI_BASE_URL to remove trailing slash
    [+] feat(constant.ts): add ServiceProvider enum with OpenAI and Azure options
    [+] feat(constant.ts): add Azure.ExampleEndpoint constants

--- app/constant.ts
+++ app/constant.ts
@@ -10,7 +10,7 @@ export const RUNTIME_CONFIG_DOM = "danger-runtime-config";
 
 export const DEFAULT_CORS_HOST = "https://chatgpt.btz.sh";
 export const DEFAULT_API_HOST = `${DEFAULT_CORS_HOST}/api/proxy/`;
-export const OPENAI_BASE_URL = `https://api.openai.com/`
+export const OPENAI_BASE_URL = "https://api.openai.com";
 
 export enum Path {
   Home = "/",
@@ -25,10 +25,12 @@ export enum Path {
 
 export enum ApiPath {
   Cors = "/api/cors",
+  OpenAI = "/api/openai",
 }
 
 export enum SlotID {
   AppBody = "app-body",
+  CustomModel = "custom-model",
 }
 // This will automatically generate JSON files without the need to include the ".json" extension.
 export enum FileName {
@@ -62,9 +64,15 @@ export const REQUEST_TIMEOUT_MS = 60000;
 
 export const EXPORT_MESSAGE_CLASS_NAME = "export-markdown";
 
+export enum ServiceProvider {
+  OpenAI = "OpenAI",
+  Azure = "Azure",
+}
+
 export const OpenaiPath = {
   ChatPath: "v1/chat/completions",
   // text moderation
+  TextModeration: true,
   ModerationPath: "v1/moderations",
   TextModerationModels: {
     latest: "text-moderation-latest",
@@ -80,6 +88,10 @@ export const OpenaiPath = {
   ListModelPath: "v1/models",
 };
 
+export const Azure = {
+  ExampleEndpoint: "https://{resource-url}/openai/deployments/{deploy-id}",
+};
+
 export const DEFAULT_INPUT_TEMPLATE = `{{input}}`; // input / time / model / lang
 export const DEFAULT_SYSTEM_TEMPLATE = `
 You are ChatGPT, a large language model trained by OpenAI.
@@ -92,7 +104,6 @@ export const SUMMARIZE_MODEL = "gpt-3.5-turbo";
 
 export const KnowledgeCutOffDate: Record<string, string> = {
   default: "2021-09",
-  "gpt-3.5-turbo-1106": "2023-04",
   "gpt-4-1106-preview": "2023-04",
   "gpt-4-vision-preview": "2023-04",
 };
@@ -172,7 +183,6 @@ export const DEFAULT_MODELS = [
     name: "gpt-3.5-turbo-16k-0613",
     available: true,
   },
-
 ] as const;
 
 export const CHAT_PAGE_SIZE = 15;
