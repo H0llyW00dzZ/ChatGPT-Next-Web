25ebd69cc8eaa4ee445f3ba5f4170961810b1703
Yidadaa <yidadaa@qq.com>
2023-11-12 19:45:58 +0800

    fix: #2841 dollar sign conflict with latex math

--- app/constant.ts
+++ app/constant.ts
@@ -97,7 +97,6 @@ You are ChatGPT, a large language model trained by OpenAI.
 Knowledge cutoff: {{cutoff}}
 Current model: {{model}}
 Current time: {{time}}
-
 Latex inline: $x^2$ 
 Latex block: $$e=mc^2$$
 `;
