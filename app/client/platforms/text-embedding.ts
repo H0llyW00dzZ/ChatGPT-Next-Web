a351977f043c1a3995cdb328d61b02aab33a2361
H0llyW00dzZ <priv8@btz.pm>
2023-12-03 23:35:43 +0700

    Feat Text Embedding CSV Files
    - [+] feat(text-embedding.ts): add support for reading CSV file, parsing it, and getting embeddings
    - [+] chore(package.json): add @types/papaparse dependency
    - [+] chore(yarn.lock): update @types/papaparse to version 5.3.14

--- app/client/platforms/text-embedding.ts
+++ app/client/platforms/text-embedding.ts
@@ -15,6 +15,12 @@ import {
     LLMApi,
     LLMModel,
     LLMUsage } from "../api";
+import Papa from "papaparse";
+
+// Define the structure of the CSV row based on the expected columns
+interface CsvRow {
+    [columnName: string]: string | undefined; // Use 'undefined' as well since some columns might be missing
+  }
 
 export async function textEmbedding(inputTexts: string[], embeddingPath: string): Promise<any> {
   try {
@@ -38,3 +44,54 @@ export async function textEmbedding(inputTexts: string[], embeddingPath: string)
     throw e;
   }
 }
+
+// This function is now responsible for reading CSV, parsing it, and getting embeddings
+export async function getTextEmbeddingsFromCSV(file: File, embeddingPath: string, columnName: string): Promise<any> {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onload = async (e) => {
+        if (e.target) {
+          const csvData = e.target.result as string;
+          Papa.parse(csvData, {
+            header: true,
+            complete: async (results) => {
+              // Assert the type of 'results.data' as 'CsvRow[]' to inform TypeScript about the expected type
+              const textsToEmbed = (results.data as CsvRow[]).map(row => row[columnName] || '');
+  
+              // Now we have the texts, let's get the embeddings
+              try {
+                const response = await fetch(embeddingPath, {
+                  method: 'POST',
+                  headers: getHeaders(),
+                  body: JSON.stringify({ input: textsToEmbed }),
+                });
+  
+                if (!response.ok) {
+                  throw new Error('Failed to get text embeddings from OpenAI');
+                }
+  
+                const embeddings = await response.json();
+                resolve(embeddings);
+              } catch (error) {
+                console.error('Failed to make a text embedding request', error);
+                reject(error);
+              }
+            },
+            error: (error: any) => {
+              console.error('Error parsing CSV file', error);
+              reject(error);
+            }
+          });
+        } else {
+          reject(new Error('FileReader event target is null'));
+        }
+      };
+
+      reader.onerror = (error) => {
+        console.error('Error reading CSV file', error);
+        reject(error);
+      };
+
+      reader.readAsText(file);
+    });
+  }
