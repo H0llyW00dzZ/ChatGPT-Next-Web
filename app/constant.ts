db5acf82b9800ef8300e6a48d06563568d1bb240
H0llyW00dzZ <priv8@btz.pm>
2024-01-26 17:09:39 +0700

    Add gpt-4-turbo-preview

--- app/constant.ts
+++ app/constant.ts
@@ -128,6 +128,7 @@ export const SUMMARIZE_MODEL = "gpt-3.5-turbo";
 
 export const KnowledgeCutOffDate: Record<string, string> = {
   default: "2021-09",
+  "gpt-4-turbo-preview": "2023-04",
   "gpt-4-1106-preview": "2023-04",
   "gpt-4-0125-preview": "2023-04",
   "gpt-4-vision-preview": "2023-04",
@@ -227,6 +228,15 @@ export const DEFAULT_MODELS = [
   },
   // recent update 
   // read here : https://openai.com/blog/new-models-and-developer-products-announced-at-devday
+  {
+    name: "gpt-4-turbo-preview",
+    available: true,
+    provider: {
+      id: "openai",
+      providerName: "OpenAI",
+      providerType: "openai",
+    },
+  },
   {
     name: "gpt-4-1106-preview",
     available: true,
